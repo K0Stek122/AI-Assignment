58cbeb2122c67ce1a87c0c0caba487ca9cd34b33
Kamil Kostrzewa <kamilianos3@gmail.com>
2025-12-01 14:06:13 +0000

    Add .gitignore to exclude NLTK data, dataset, and text files

--- kmeans.py
+++ kmeans.py
@@ -16,6 +16,7 @@ def setup_arguments():
     parser.add_argument('-v', '--verbose', help="Show each k-means algorithm run.", action='store_true')
     parser.add_argument('-c', '--count_words', help="Counts words in a sentence and outputs to the console or file.", action='store_true')
     parser.add_argument('-o', '--output', help="Outputs contents to a file.")
+    parser.add_argument('-cc', '--clear-cache', help="Clears the NLTK download cache", action='store_true')
     return parser.parse_args()
 
 def tokenise(df : pd.DataFrame, args):
@@ -23,16 +24,12 @@ def tokenise(df : pd.DataFrame, args):
     lemmatizer = WordNetLemmatizer()
     for review in list(df['review'] ):
         sentence = nltk.sent_tokenize(review)
-        print(sentence)
         words = nltk.word_tokenize(review)
         for word in words:
             base_word = lemmatizer.lemmatize(word)
             word_count.append(base_word)
     return word_count
          
-        
-        
-
 def open_csv(filename : str):
     if not filename.endswith('.csv'):
         return pd.DataFrame()
@@ -41,11 +38,20 @@ def open_csv(filename : str):
     df = pd.read_csv(filename)
     return df
 
+def download_nltk_packages(args):
+    nltk.data.clear_cache() if args.clear_cache else False
+
+    nltk_path = os.path.join(os.getcwd(), "nltk_data/")
+    os.makedirs(nltk_path, exist_ok=True)
+    nltk.data.path = [nltk_path]
+
+    packages = ['punkt_tab', 'wordnet', 'averaged_perceptron_tagger', 'punkt']
+    for package in packages:
+        nltk.download(package, download_dir=nltk_path)
+
 if __name__ == "__main__":
-    nltk.download('all')
-    nltk.download('punkt_tab')
-    nltk.download('wordnet')
     args = setup_arguments()
+    download_nltk_packages(args)
     df = open_csv(args.input)
     if df.empty:
         print(f"[ERROR] {args.input} does not exist or isn't a csv file!")
